6f65f3348317f1a95d59a481dee24136ed9c0dd7
iMidnights <iMidnights@users.noreply.github.com>
2022-10-15 16:59:49 +0000

    deploy: 9fd828d2229320f3b032e65b21d73c03fae9423a

--- assets/settings_index.md.d6bbfa39.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as n,o as a,a as p}from"./app.a10d8fc7.js";const A=JSON.parse('{"title":"Settings","description":"","frontmatter":{},"headers":[],"relativePath":"settings/index.md","lastUpdated":1665851892000}'),l={name:"settings/index.md"},e=p("",3),o=[e];function t(r,c,i,D,y,F){return a(),n("div",null,o)}const b=s(l,[["render",t]]);export{A as __pageData,b as default};
