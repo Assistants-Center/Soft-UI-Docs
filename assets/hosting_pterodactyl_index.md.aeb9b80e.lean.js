d6c7253201771575bc14ddbe160b4629451246db
iMidnights <iMidnights@users.noreply.github.com>
2022-11-23 20:09:57 +0000

    deploy: 57eb94729d8f468c0d52154c41bcacd5af853fa4

--- assets/hosting_pterodactyl_index.md.aeb9b80e.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as e,o as a}from"./app.4bc5dcb2.js";const l=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"hosting/pterodactyl/index.md","lastUpdated":1667854619000}'),o={name:"hosting/pterodactyl/index.md"};function n(r,s,c,d,i,p){return a(),e("div")}const m=t(o,[["render",n]]);export{l as __pageData,m as default};
