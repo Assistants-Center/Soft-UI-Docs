6c0f534e8a6e1bfc39c253590cf3dd35a6766481
iMidnights <iMidnights@users.noreply.github.com>
2023-01-26 19:54:30 +0000

    deploy: b80761a6b9b203f7ae382d1cb3742f9e396ab5c4

---
+++ assets/hosting_pterodactyl_index.md.a039f020.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as a}from"./app.1ffdc0b6.js";const l=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"hosting/pterodactyl/index.md","lastUpdated":1674762832000}'),o={name:"hosting/pterodactyl/index.md"};function n(r,s,c,d,i,p){return a(),e("div")}const m=t(o,[["render",n]]);export{l as __pageData,m as default};
---
+++ assets/intro_index.md.e211a4c2.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o,a}from"./app.1ffdc0b6.js";const _=JSON.parse('{"title":"Introduction","description":"","frontmatter":{},"headers":[],"relativePath":"intro/index.md","lastUpdated":1674762832000}'),r={name:"intro/index.md"},n=a("",6),i=[n];function s(l,d,c,u,m,p){return o(),t("div",null,i)}const f=e(r,[["render",s]]);export{_ as __pageData,f as default};
